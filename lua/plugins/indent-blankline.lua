f334e654d6cd6559ec5cac3b703d702cca59d48f
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-07-16 21:48:19 -0300

    upgrade indent

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -24,8 +24,9 @@ return {
             vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
             vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
         end)
-        require("ibl").setup({
-            indent = { highlight = highlight },
-        })
+        vim.g.rainbow_delimiters = { highlight = highlight }
+        require("ibl").setup({ scope = { highlight = highlight } })
+
+        hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
 }

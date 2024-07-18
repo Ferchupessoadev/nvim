cc67492d9292a94e226b8fc02b00e2987537220d
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-07-18 08:10:33 -0300

    Update: formatting.lua and linting.lua

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,38 +1,39 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-        conform.setup({
-            formatters_by_ft = {
-                astro = { "prettier" },
-                php = { "php-cs-fixer" },
-                javascript = { "prettierd" },
-                typescript = { "prettierd" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                svelte = { "prettier" },
-                css = { "prettierd" },
-                html = { "prettierd" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                markdown = { "prettier" },
-                graphql = { "prettier" },
-                lua = { "stylua" },
-                python = { "isort", "black" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-            },
-        })
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+		conform.setup({
+			formatters_by_ft = {
+				astro = { "prettier" },
+				php = { "php-cs-fixer" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				arduino = { "clangd" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }

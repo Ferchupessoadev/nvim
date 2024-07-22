590cbdbdeb39fdcaa941c82a30b49178bd42f7ab
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-07-22 19:36:55 -0300

    up alpha

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,7 +3,13 @@ return {
     config = function()
         local notify = require("notify")
         -- this for transparency
-        notify.setup({ background_colour = "#000000" })
+        notify.setup({
+            background_colour = "#000000",
+            render = "compact",
+            stages = "static",
+            timeout = 1000,
+            fps = 10,
+        })
         -- this overwrites the vim notify function
         vim.notify = notify.notify
     end,

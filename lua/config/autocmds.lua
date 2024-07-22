1aac5d46179ff8f1468f68ecb7d52f062ae5720e
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-07-22 19:49:21 -0300

    added autocmd for compile dwmblocks

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,4 @@
+-- compile dwmblocks
+vim.cmd(
+    [[autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & ]]
+)

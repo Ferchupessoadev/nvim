da7bd6f4541c9aa847253dc007b1de9a5de75cbf
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-08-03 19:57:26 -0300

    removed events in formatting and added git status

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,6 +1,6 @@
 return {
     "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    event = {},
     config = function()
         local conform = require("conform")
 
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
     "tpope/vim-fugitive",
     cmd = { "G", "Git" },
     keys = {
+		{ "<leader>gs", ":G<cr>", desc = "Git" },
         { "<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch" },
         { "<leader>gl", ":Git pull<cr>", desc = "Git pull" },
         { "<leader>gi", ":Git init<cr>", desc = "Git init" },

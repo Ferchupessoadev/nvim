8092f36a5e8e6f78f76a47cd4898e5e7811cd913
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-08-11 16:32:38 -0300

    keymap for alpha-nvim

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -3,5 +3,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("alpha").setup(require("alpha.themes.startify").config)
+        -- toggle alpha
+        vim.keymap.set("n", "<C-A-a>", "<cmd>Alpha<CR>", { desc = "Alpha nvim" })
     end,
 }

92713a177e9d38a29de7429bfcc86289641ea7b6
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-08-11 16:25:38 -0300

    more autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,14 +6,32 @@ vim.cmd(
 vim.api.nvim_create_user_command("UploadArduino", function()
     -- Mostrar notificación inicial
     vim.notify("Compilando...", vim.log.levels.INFO, { timeout = 3000 })
-
     -- Ejecutar la tarea externa
     vim.fn.system("make upload")
 
     -- Usar `vim.schedule` para mostrar una nueva notificación
     vim.schedule(function()
-        vim.notify("Compilación completada", vim.log.levels.INFO, { timeout = 2000 })
+        vim.notify("Compilación y subida completada", vim.log.levels.INFO, { timeout = 2000 })
     end)
 end, {})
 
+-- tab stop for the file extension
 vim.keymap.set("n", "<leader>ua", ":UploadArduino<cr>", { desc = "Upload arduino" })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "html", "css" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "php", "javascript", "typescript", "typescriptreact", "javascriptreact", "astro", "python" },
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.softtabstop = 4
+        vim.opt_local.shiftwidth = 4
+    end,
+})

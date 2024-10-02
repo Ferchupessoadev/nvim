d15cb59dea901a946c9c5edda7098b0de559da7e
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-10-02 20:53:20 -0300

    adjust toogleterm and formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -24,7 +24,7 @@ return {
             format_on_save = {
                 lsp_fallback = true,
                 async = false,
-                timeout_ms = 500,
+                timeout_ms = 1000,
             },
         })
 
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,7 +11,7 @@ return {
             start_in_insert = true,
             insert_mappings = true,
             persist_size = true,
-            direction = "horizontal",
+            direction = "float",
             close_on_exit = true,
             shell = vim.o.shell,
             float_opts = {
